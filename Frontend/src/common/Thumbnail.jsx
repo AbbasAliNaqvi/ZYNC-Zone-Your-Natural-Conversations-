d46be121420ac4324b1eae5b9c58a5c84e285a20
AbbasAliNaqvi <abbasalinaqvi888@gmail.com>
2025-01-15 14:23:56 +0530

    MARK XXII

--- Frontend/src/common/Thumbnail.jsx
+++ Frontend/src/common/Thumbnail.jsx
@@ -1,19 +1,22 @@
-import { Image } from "react-native"
-import utlis from "../core/utlis"
+import { Image, View } from "react-native";
+import React from 'react';
+import useGlobal from '../core/global'
 
-function Thumbnail({ url, size }) {
-	return (
-	console.log(url),
-	<Image 
-		source={utlis.thumbnail(url)}
-			style={{ 
-				width: size, 
-				height: size, 
-				borderRadius: size / 2,
-				backgroundColor: '#e0e0e0' 
-			}}
-	/>
-	)
+function Thumbnail({ url, size, style }) {
+  const user = useGlobal((state) => state.user);
+  const baseUrl = 'http://192.168.1.6:8000';
+  
+  const thumbnailUrl = url || user?.thumbnail;
+  const fullUrl = thumbnailUrl?.startsWith('http') ? thumbnailUrl : `${baseUrl}${thumbnailUrl}`;
+
+  return (
+    <View style={[{ width: size, height: size, backgroundColor: '#f3f4f6' }, style]}>
+      <Image 
+        source={{ uri: fullUrl }} 
+        style={{ width: size, height: size, borderRadius: size / 2 }}
+      />
+    </View>
+  );
 }
 
-export default Thumbnail
+export default Thumbnail;
